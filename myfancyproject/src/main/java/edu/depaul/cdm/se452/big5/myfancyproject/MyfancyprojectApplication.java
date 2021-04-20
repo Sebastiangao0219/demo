527eada55c0404a371884d04f0a9adb86ff433e5
SebastianGao <53056387+Sebastiangao0219@users.noreply.github.com>
2021-04-20 00:41:05 -0500

    first spring project commit

---
+++ myfancyproject/src/main/java/edu/depaul/cdm/se452/big5/myfancyproject/MyfancyprojectApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.big5.myfancyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfancyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfancyprojectApplication.class, args);
+	}
+
+}
---
+++ myfancyproject/src/test/java/edu/depaul/cdm/se452/big5/myfancyproject/MyfancyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.big5.myfancyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
